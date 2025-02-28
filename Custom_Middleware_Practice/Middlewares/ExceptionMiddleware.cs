b3e3b721a93472ec941af866bfce3d2d6821b885
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2025-02-28 21:05:18 +0530

    Created custom Exception Middleware

---
+++ Custom_Middleware_Practice/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,35 @@
+﻿namespace Custom_Middleware_Practice.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly ILogger<ExceptionMiddleware> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+
+
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context); // Pass request to next middleware
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception: {ex.Message}"); // Log the error
+                context.Response.StatusCode = 500; // Internal Server Error
+                context.Response.ContentType = "application/json";
+
+                var errorResponse = new { message = "Internal Server Error. Please try again later." };
+                await context.Response.WriteAsJsonAsync(errorResponse);
+            }
+
+        }
+
+    }
+}
--- Custom_Middleware_Practice/Program.cs
+++ Custom_Middleware_Practice/Program.cs
@@ -1,3 +1,5 @@
+using Custom_Middleware_Practice.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
