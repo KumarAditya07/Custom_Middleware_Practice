78c1ee040da37d150b51611c00c27667d530e66e
Kumar_Aditya(2121841) <18bcs4266@gmail.com>
2025-03-01 11:17:54 +0530

    Created Product controller for tesing purpose

---
+++ Custom_Middleware_Practice/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Custom_Middleware_Practice.Controllers
+{
+    public class ProductController : ControllerBase
+    {
+
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = Guid.NewGuid().ToString();
+            if (product != null)
+                return NotFound("Product not found");
+
+           return Ok("Hellow Wold");
+        }
+
+    }
+}
